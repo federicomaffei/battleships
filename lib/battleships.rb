6b757f1fb00e1306c15b82362be2adb1d9baeb24
federicomaffei <federico.maffei@gmail.com>
2014-06-11 17:10:57 +0100

    added target coordinate input method

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -14,12 +14,28 @@ class BattleShips
 	end
 
 	def prompt_ships(player)
-		prompt = "#{player.name}: Please place a ship on the board"
+		prompt = "#{player.name}: Please name a ship to place on the board"
 		puts prompt
 	end
+
+	def prompt_coordinates(player)
+		prompt = "#{player.name}: Please name a coordinate to place the ship on"
+		puts prompt
+	end
+
+	def prompt_target(player)
+		prompt = "#{player.name}: Please enter a coordinate to target"
+		puts prompt
+	end
+
+	def get_data
+		STDIN.gets.chomp
+	end	
 
 	def players_swap
 		@player1.active, @player2.active = @player2.active, @player1.active
 	end
+
+
 
 end
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -21,13 +21,26 @@ describe BattleShips do
 
 	context 'ships placing' do
 
-		it 'asks the user to place ships' do
-			prompt = "Player 1: Please place a ship on the board"
+		it 'asks the player to place ships by name' do
+			prompt = "Player 1: Please name a ship to place on the board"
 			expect(STDOUT).to receive(:puts).with prompt
 			battleships.prompt_ships(battleships.player1)
-			prompt = "Player 2: Please place a ship on the board"
+		end
+
+		it 'asks the player to place ships by coordinate' do
+			prompt = "Player 1: Please name a coordinate to place the ship on"
 			expect(STDOUT).to receive(:puts).with prompt
-			battleships.prompt_ships(battleships.player2)
+			battleships.prompt_coordinates(battleships.player1)
+		end
+
+		it 'allows the player to give the name of the ships' do	
+			expect(STDIN).to receive(:gets).and_return("carrier\n")
+			expect(battleships.get_data).to eq 'carrier'
+		end
+
+		it 'allows the player to give the coordinate of the ships' do	
+			expect(STDIN).to receive(:gets).and_return("A1\n")
+			expect(battleships.get_data).to eq 'A1'
 		end
 
 	end
@@ -40,10 +53,13 @@ describe BattleShips do
 			expect(battleships.player2).to be_active
 		end
 
+		it 'asks the player for a coordinate to target' do
+			prompt = "Player 1: Please enter a coordinate to target"
+			expect(STDOUT).to receive(:puts).with prompt
+			battleships.prompt_target(battleships.player1)
+		end
+
 	end
-
-
-
 
 end
 
